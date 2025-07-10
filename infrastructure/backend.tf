8933356c4e1d980da8146cce01a6ad3ef44bd54e
jsoria21 <jorge.soria01@gmail.com>
2025-07-09 19:41:23 -0500

    Update backend.tf

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,1 +1,7 @@
-
+terraform {
+  backend "azurerm" {
+    storage_account_name = "storagesample01"
+    container_name       = "webappstate"
+    key                  = "stateActions.tfstate"
+  }
+}
